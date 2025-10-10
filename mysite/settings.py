481acae5f63347a56b3ba984f6e1cf7dc4359fa4
Finch Kyle <aron.ngenok@gmail.com>
2025-10-10 10:36:45 +0300

    Connect app to Postgresql database

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kyle',
+        'USER': 'postgre',
+        'PASSWORD': '$Kylean1',
+        'HOST': 'localhost'
     }
 }
 
