715670d5df7d37d00ff00486550b1eab659748f5
Finch Kyle <aron.ngenok@gmail.com>
2025-11-03 21:32:57 +0300

    Create Personal Django forms and link it to the blog page

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    tittle = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
@@ -18,4 +18,4 @@ class Post(models.Model):
 
 
     def __str__(self):
-        return self.tittle
+        return self.title  
